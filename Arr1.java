7a5a09ed29b53127372522e635ea4346356901aa
Nihma <Nihmapk@gmail.com>
2021-10-08 16:45:44 +0530

    Arr1.java

---
+++ Arr1.java
@@ -0,0 +1,17 @@
+
+public class Arr1 {
+
+	public static void main(String[] args)
+	{
+		int x[]= {70,80,90};
+	/*	int y[]= {77,89,99};
+		int z[]= {};*/
+		int sum=0;
+		for(int i=0;i<4;i++)
+		{
+			sum=sum+x[i];  // sum of an array
+		
+	}
+		System.out.println("sum is" +sum);
+
+}}
